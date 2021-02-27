fe4fab77cd15790ade6f0d0d596ae58896f58f74
Melvyn <melvyn.fontaine@epitech.eu>
2021-02-27 17:47:53 +0100

    [ADD] passeport github + gitlab

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const cookieSession = require('cookie-session')
+const passport = require('passport');
+require('./passport')
+const isLoggedIn = require('./Middleware/auth')
+
+app.use(cookieSession({
+  name: 'facebook-auth-session',
+  keys: ['key1', 'key2']
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',isLoggedIn,(req,res)=>{
+  res.send(`Hello world ${req.user.displayName}`)
+})
+app.get('/auth/error', (req, res) => res.send('Unknown Error'))
+app.get('/auth/github',passport.authenticate('github',{ scope: [ 'user:email' ] }));
+app.get('/auth/github/callback',passport.authenticate('github', { failureRedirect: '/auth/error' } ),
+function(req, res) {
+   console.log("LALA");
+
+});
+
+app.get('/auth/gitlab', passport.authenticate('gitlab', {scope: ['api']}));
+app.get('/auth/gitlab/callback',
+  passport.authenticate('gitlab', {
+    failureRedirect: '/login'
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+})
+app.listen(8000,()=>{
+  console.log('Serve is up and running at the port 8000')
+})
---
+++ passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+const GitLabStrategy = require('passport-gitlab2').Strategy;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: "", // put your Github client ID
+    clientSecret: "", // put your Github client secret
+    callbackURL: "http://localhost:8000/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+    console.log(accessToken)
+    return done(null, profile);
+    }
+));
+
+passport.use(new GitLabStrategy({
+  clientID: '', // put your GitLab client id
+  clientSecret: '', // put your GitLab client secret
+  callbackURL: "http://localhost:8000/auth/gitlab/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log('GitLab accessToken: ' , accessToken)
+  // console.log('GitLab profile: ' , profile)
+  // console.log('GitLab cb: ' , cb)
+  User.findOrCreate({gitlabId: profile.id}, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
